d537554583672dfe6e995744721a5502712d40d3
Ptorresr <pedro.torres.rdz@gmail.com>
2020-05-25 22:46:26 +0000

    Port change to 8005

--- promotion.py
+++ promotion.py
@@ -29,7 +29,7 @@ def conn():
 
 
 if __name__ == '__main__':
-    run(app, host='0.0.0.0', port=8080)
+    run(app, host='0.0.0.0', port=8005)
     DBUSER = 'hr'
     DBPASS = 'OraPTS#2020_'
     DBHOST = '130.61.182.140'
