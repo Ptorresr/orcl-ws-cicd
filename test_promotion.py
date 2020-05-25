b6cc7dce237a60e6604f4fe5c535aa32361bd65a
Ptorresr <pedro.torres.rdz@gmail.com>
2020-05-25 20:19:36 +0000

    Add unit tests for web service

--- test_promotion.py
+++ test_promotion.py
@@ -24,9 +24,11 @@ def application():
     test_app = TestApp(promotion.app)
     return test_app
 
+
 def test_response_shold_be_ok(application):
     response = application.get('/addition/1000/200')
     assert response.status == "200 OK"
+
 
 def test_addition(application):
     response = application.get('/addition/1000/200')
