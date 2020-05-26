09f5c77e9a37e5f178011e547d9d91249ed38883
Ptorresr <pedro.torres.rdz@gmail.com>
2020-05-26 15:39:43 +0000

    Add database connection unit test

--- test_promotion.py
+++ test_promotion.py
@@ -5,6 +5,7 @@ Unit tests for simple Python application
 import promotion
 import pytest
 from webtest import TestApp
+import cx_Oracle
 
 
 class TestPromotion:
@@ -33,3 +34,17 @@ def test_response_shold_be_ok(application):
 def test_addition(application):
     response = application.get('/addition/1000/200')
     assert b'1200' == response.body
+
+
+@pytest.fixture(scope='session')
+def test_connection():
+    DBUSER = 'hr'
+    DBPASS = 'OraPTS#2020_'
+    DBHOST = '130.61.182.140'
+    DBSERV = 'pdb03.sub05131148020.holvcn.oraclevcn.com'
+    conn_string = DBUSER + '/' + DBPASS + '@//' + DBHOST + '/' + DBSERV
+    connection = cx_Oracle.connect(conn_string)
+    response = connection.version
+    assert response == '19.6.0.0.0'
+    connection.close()
+
