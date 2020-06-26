ef154a9bb688260cb6209f994607ffffc816f6fd
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 15:04:32 +0000

    Add automatic testing

--- test_promotion.py
+++ test_promotion.py
@@ -3,47 +3,12 @@ Unit tests for simple Python application
 """
 
 import promotion
-import pytest
-from webtest import TestApp
-import cx_Oracle
 
 
 class TestPromotion:
 
     def test_addition(self):
-        assert '1200' == promotion.addition(1150, 50)
+        assert 1200 == promotion.addition(1150, 50)
 
     def test_increment(self):
-        assert '1250.0' == promotion.increment(1000, 25)
-
-    def test_decrease(self):
-        assert '970' == promotion.decrease(1150, 180)
-
-
-@pytest.fixture
-def application():
-    test_app = TestApp(promotion.app)
-    return test_app
-
-
-def test_response_shold_be_ok(application):
-    response = application.get('/addition/1000/200')
-    assert response.status == "200 OK"
-
-
-def test_addition(application):
-    response = application.get('/addition/1000/200')
-    assert b'1200' == response.body
-
-
-@pytest.fixture(scope='session')
-def test_connection():
-    DBUSER = 'hr'
-    DBPASS = 'OraPTS#2020_'
-    DBHOST = '130.61.182.140'
-    DBSERV = 'pdb03.sub05131148020.holvcn.oraclevcn.com'
-    conn_string = DBUSER + '/' + DBPASS + '@//' + DBHOST + '/' + DBSERV
-    connection = cx_Oracle.connect(conn_string)
-    response = connection.version
-    assert response == '19.6.0.0.0'
-    connection.close()
+        assert 1250 == promotion.increment(1000, 25)
