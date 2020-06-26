ac2e8a9de42c05170109aa1b4536c08ec32dba9f
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 15:51:29 +0000

    Add data type conversions

--- test_promotion.py
+++ test_promotion.py
@@ -9,32 +9,33 @@ from webtest import TestApp
 
 class TestPromotion:
 
-@app.route('/addition/<salary>/<amount>')
-def addition(salary, amount):
+
+ @app.route('/addition/<salary>/<amount>')
+ def addition(salary, amount):
     return str(int(salary) + int(amount))
 
 
-@app.route('/increment/<salary>/<percentage>')
-def increment(salary, percentage):
+ @app.route('/increment/<salary>/<percentage>')
+ def increment(salary, percentage):
     return str(int(salary) * (1 + int(percentage)/100))
 
 
-@app.route('/decrease/<salary>/<amount>')
-def decrease(salary, amount):
+ @app.route('/decrease/<salary>/<amount>')
+ def decrease(salary, amount):
     return str(int(salary) - int(amount))
 
 
-@pytest.fixture
-def application():
+ @pytest.fixture
+ def application():
     test_app = TestApp(promotion.app)
     return test_app
 
 
-def test_response_shold_be_ok(application):
+ def test_response_shold_be_ok(application):
     response = application.get('/addition/1000/200')
     assert response.status == "200 OK"
 
 
-def test_addition(application):
+ def test_addition(application):
     response = application.get('/addition/1000/200')
     assert b'1200' == response.body
