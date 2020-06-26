0d2a16a6910b01fdf9bf15e913102be1a42ceeb8
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 16:07:58 +0000

    Add unit tests for web service

--- test_promotion.py
+++ test_promotion.py
@@ -10,13 +10,13 @@ from webtest import TestApp
 class TestPromotion:
 
 
- def test_addition(self):
+def test_addition(self):
       assert '1200' == promotion.addition(1150, 50)
 
- def test_increment(self):
+def test_increment(self):
       assert '1250.0' == promotion.increment(1000, 25)
 
- def test_decrease(self):
+def test_decrease(self):
       assert '970' == promotion.decrease(1150, 180)
 
 
