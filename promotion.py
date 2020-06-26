b0ddcb2b8ac07f33344be6da62000d4dfda78070
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 17:34:28 +0000

    Add database connection

--- promotion.py
+++ promotion.py
@@ -2,6 +2,7 @@
 Simple Python application to show CI/CD capabilities.
 """
 
+import cx_Oracle
 from bottle import Bottle, run
 
 app = Bottle()
@@ -22,5 +23,18 @@ def decrease(salary, amount):
     return str(int(salary) - int(amount))
 
 
+@app.route('/conn')
+def conn():
+    return str(connection.version)
+
+
 if __name__ == '__main__':
     run(app, host='0.0.0.0', port=8007)
+    DBUSER = 'hr'
+    DBPASS = 'OraPTS#2020_'
+    DBHOST = '130.61.252.65'
+    DBSERV = 'pdb03.holpublic.holvcn.oraclevcn.com'
+    conn_string = DBUSER + '/' + DBPASS + '@//' + DBHOST + '/' + DBSERV
+    connection = cx_Oracle.connect(conn_string)
+    run(app, host='0.0.0.0', port=8007)
+    connection.close()
